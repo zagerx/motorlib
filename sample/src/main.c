bd3460ecd6de0598a6a302c7f9d6d653e055798c
zhangge <zhangge@syriusrobotics.com>
2025-08-26 18:19:34 +0800

    增加sample模块

---
+++ sample/src/main.c
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2019 Vestas Wind Systems A/S
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
+
+#define LOG_LEVEL LOG_LEVEL_DBG
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(motor_sample);
+int main(void)
+{
+	// 主处理循环
+	while (true) {
+	}
+}
