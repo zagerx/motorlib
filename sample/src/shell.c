1be18cf00f4a679a8bba385bbed57dad1bf5667f
zhangge <zhangge@syriusrobotics.com>
2025-09-09 13:44:34 +0800

    优化shell接口

--- sample/src/shell.c
+++ sample/src/shell.c
@@ -4,6 +4,7 @@
 #include <zephyr/drivers/gpio.h>
 
 #include "lib/motor/motor.h"
+#include "zephyr/posix/sys/stat.h"
 const struct device *motor = DEVICE_DT_GET(DT_NODELABEL(motor0));
 
 static int cmd_hello(const struct shell *sh, size_t argc, char **argv)
@@ -107,6 +108,37 @@ static int cmd_motor_set_posi(const struct shell *sh, size_t argc, char **argv)
 	motor_set_state(motor, MOTOR_STATE_CLOSED_LOOP);
 	return 0;
 }
+static int cmd_motor_get_modestate(const struct shell *sh, size_t argc, char **argv)
+{
+	if (argc != 2) {
+		shell_error(sh, "Usage: add <num1> <num2>");
+		return -EINVAL;
+	}
+	long state = strtol(argv[1], NULL, 0);
+	if (state == 1) {
+		if (motor_get_state(motor) == MOTOR_STATE_READY) {
+			shell_print(sh, "motor_ready_state");
+		} else if (motor_get_state(motor) == MOTOR_STATE_CLOSED_LOOP) {
+			shell_print(sh, "motor_runing_state");
+		} else if (motor_get_state(motor) == MOTOR_STATE_STOP) {
+			shell_print(sh, "motor_stop_state");
+		} else if (motor_get_state(motor) == MOTOR_STATE_IDLE) {
+			shell_print(sh, "motor_idle_state");
+		} else if (motor_get_state(motor) == MOTOR_STATE_FAULT) {
+			shell_print(sh, " motor_falut_state");
+		} else if (motor_get_state(motor) == MOTOR_STATE_ALIGN) {
+			shell_print(sh, " motor_aligh_state");
+		}
+	} else if (state == 2) {
+		if (motor_get_mode(motor) == MOTOR_MODE_TORQUE) {
+			shell_print(sh, " motor_torque_control_mode");
+		} else if (motor_get_mode(motor) == MOTOR_MODE_SPEED) {
+			shell_print(sh, "motor_speed_control_mode");
+		} else if (motor_get_mode(motor) == MOTOR_MODE_POSI) {
+			shell_print(sh, "motor_position_control_mode");
+		}
+	}
+}
 SHELL_CMD_REGISTER(hello, NULL, "Print hello", cmd_hello);
 SHELL_CMD_REGISTER(mode, NULL, "Motor Set Mode", cmd_motor_set_mode);
 SHELL_CMD_REGISTER(r, NULL, "Motor Set Ready", cmd_motor_set_ready);
@@ -116,3 +148,4 @@ SHELL_CMD_REGISTER(speed, NULL, "Motor Set Speed", cmd_motor_set_speed);
 SHELL_CMD_REGISTER(s, NULL, "Motor Set Stop", cmd_motor_set_disable);
 SHELL_CMD_REGISTER(p, NULL, "Motor Set Posi", cmd_motor_set_posi);
 SHELL_CMD_REGISTER(t, NULL, "Motor Set Toque", cmd_motor_set_toque);
+SHELL_CMD_REGISTER(g, NULL, "Motor Get State", cmd_motor_get_modestate);
