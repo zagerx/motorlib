a208fa634adfe0de09d8a63de64b1ef8d3a412f5
zhangge <1252496768@qq.com>
2025-09-11 00:25:48 +0800

    优化电流采样驱动模块

--- drivers/currsmp/currsmp_shunt_eulre_hal.c
+++ drivers/currsmp/currsmp_shunt_eulre_hal.c
@@ -21,6 +21,10 @@ LOG_MODULE_REGISTER(currsmp_shunt_stm32);
 struct currsmp_shunt_stm32_config {
 	/** ADC peripheral instance. */
 	ADC_TypeDef *adc;
+	/** Clock configuration for ADC. */
+	struct stm32_pclken pclken;
+	/** Pin control configuration. */
+	const struct pinctrl_dev_config *pcfg;
 	void (*irq_cfg_func)(void);
 };
 
@@ -37,18 +41,25 @@ struct currsmp_shunt_stm32_data {
 	/** ADC channel data for phase C. */
 	uint32_t adc_channl_c;
 };
-/** @brief ADC interrupt service routine.
- *
- * @param[in] dev Current sampling device.
- */
-void HAL_ADCEx_InjectedConvCpltCallback(ADC_HandleTypeDef *hadc)
+
+static int currsmp_shunt_stm32_init(const struct device *dev)
 {
-	static float iabc[3];
-	if (hadc->Instance == ADC1) {
-		iabc[0] = -((int16_t)(hadc->Instance->JDR1) - 1970) * 0.02197f;
-		iabc[2] = -((int16_t)(hadc->Instance->JDR2) - 1980) * 0.02197f;
+	const struct currsmp_shunt_stm32_config *cfg = dev->config;
+	int ret;
+
+	const struct device *clk;
+	clk = DEVICE_DT_GET(STM32_CLOCK_CONTROL_NODE);
+	ret = clock_control_on(clk, (clock_control_subsys_t *)&cfg->pclken);
+	if (ret < 0) {
+		return ret;
 	}
-	LOG_DBG("...");
+
+	ret = pinctrl_apply_state(cfg->pcfg, PINCTRL_STATE_DEFAULT);
+	if (ret < 0) {
+		return ret;
+	}
+
+	return 0;
 }
 
 static void currsmp_shunt_stm32_configure(const struct device *dev,
@@ -73,7 +84,21 @@ static void currsmp_shunt_stm32_get_bus_vol_curr(const struct device *dev, float
 						 float *bus_curr)
 {
 	*bus_vol = 24.0f;
+	*bus_curr = 0.0f;
 	return;
+}
+static void inline adc_stm32_isr(const struct device *dev)
+{
+	const struct currsmp_shunt_stm32_config *cfg = dev->config;
+	struct currsmp_shunt_stm32_data *data = dev->data;
+	if (LL_ADC_IsActiveFlag_JEOS(cfg->adc)) {
+
+		if (data->regulation_cb) {
+			// data->regulation_cb(data->regulation_ctx);
+		}
+
+		LL_ADC_ClearFlag_JEOS(cfg->adc);
+	}
 }
 
 /** @brief STM32 Shunt Current Sampling Driver API. */
@@ -83,33 +108,58 @@ static const struct currsmp_driver_api currsmp_shunt_stm32_driver_api = {
 	.get_bus_volcurr = currsmp_shunt_stm32_get_bus_vol_curr,
 };
 
-extern void MX_ADC1_Init(void);
-extern ADC_HandleTypeDef hadc1;
-extern void MX_OPAMP1_Init(void);
-extern void MX_OPAMP2_Init(void);
-extern void MX_OPAMP3_Init(void);
-static int currsmp_shunt_stm32_init(const struct device *dev)
-{
-	const struct currsmp_shunt_stm32_config *cfg = dev->config;
-	MX_ADC1_Init();
-	MX_OPAMP1_Init();
-	MX_OPAMP2_Init();
-	MX_OPAMP3_Init();
-#if CONFIG_SOC_STM32G431XX
-	LL_ADC_StartCalibration(cfg->adc, LL_ADC_SINGLE_ENDED);
-#endif
-	HAL_ADC_Start(&hadc1);
-	HAL_ADCEx_InjectedStart_IT(&hadc1);
-	return 0;
-}
+/** @internal Helper macro to define the first instance with a specific IRQn. */
+#define FIRST_WITH_IRQN_INTERNAL(n, irqn)                                                          \
+	COND_CODE_1(IS_EQ(irqn, DT_IRQN(DT_INST_PARENT(n))), (n, ), (EMPTY, ))
 
+/** @internal Helper macro to get the first instance with a specific IRQn. */
+#define FIRST_WITH_IRQN(n)                                                                         \
+	GET_ARG_N(1, LIST_DROP_EMPTY(DT_INST_FOREACH_STATUS_OKAY_VARGS(                            \
+			     FIRST_WITH_IRQN_INTERNAL, DT_IRQN(DT_INST_PARENT(n)))))
+
+/** @internal Helper macro to handle IRQs for a specific instance. */
+#define HANDLE_IRQS(n, irqn)                                                                       \
+	COND_CODE_1(IS_EQ(irqn, DT_IRQN(DT_INST_PARENT(n))),                                       \
+		    (adc_stm32_isr(DEVICE_DT_INST_GET(n));), (EMPTY))
+
+/** @internal Helper macro to generate ISR code for a specific instance. */
+#define ISR_FUNC(n) UTIL_CAT(adc_stm32_isr_, DT_IRQN(DT_INST_PARENT(n)))
+
+/** @internal Generate ISR code for a specific instance. */
+#define GENERATE_ISR_CODE(n)                                                                       \
+	static void ISR_FUNC(n)(void)                                                              \
+	{                                                                                          \
+		DT_INST_FOREACH_STATUS_OKAY_VARGS(HANDLE_IRQS, DT_IRQN(DT_INST_PARENT(n)))         \
+	}                                                                                          \
+                                                                                                   \
+	static void UTIL_CAT(ISR_FUNC(n), _init)(void)                                             \
+	{                                                                                          \
+		IRQ_CONNECT(DT_IRQN(DT_INST_PARENT(n)), DT_IRQ(DT_INST_PARENT(n), priority),       \
+			    ISR_FUNC(n), NULL, 0);                                                 \
+		irq_enable(DT_IRQN(DT_INST_PARENT(n)));                                            \
+	}
+
+/** @internal Generate ISR code for a specific instance. */
+#define GENERATE_ISR(n) COND_CODE_1(IS_EQ(n, FIRST_WITH_IRQN(n)), (GENERATE_ISR_CODE(n)), (EMPTY))
+
+/** @internal Generate device instances. */
+DT_INST_FOREACH_STATUS_OKAY(GENERATE_ISR)
+
+/** @internal Helper macro to define the IRQ function for a specific instance. */
+#define CURRSMP_SHUNT_STM32_IRQ_FUNC(n)                                                            \
+	.irq_cfg_func =                                                                            \
+		COND_CODE_1(IS_EQ(n, FIRST_WITH_IRQN(n)), (UTIL_CAT(ISR_FUNC(n), _init)), (NULL)),
+
+/** @brief Device instance definition for STM32 shunt current sampling driver. */
 #define CURRSMP_SHUNT_STM32_CURRSMP_SHUNT_INIT(n)                                                  \
                                                                                                    \
+	PINCTRL_DT_INST_DEFINE(n);                                                                 \
 	static const struct currsmp_shunt_stm32_config currsmp_shunt_stm32_config_##n = {          \
 		.adc = (ADC_TypeDef *)DT_REG_ADDR(DT_INST_PARENT(n)),                              \
-	};                                                                                         \
-	static struct currsmp_shunt_stm32_data currsmp_shunt_stm32_data_##n;                       \
-                                                                                                   \
+		.pclken = STM32_CLOCK_INFO(0, DT_INST_PARENT(n)),                                  \
+		.pcfg = PINCTRL_DT_INST_DEV_CONFIG_GET(n),                                         \
+		CURRSMP_SHUNT_STM32_IRQ_FUNC(n)};                                                  \
+	static struct currsmp_shunt_stm32_data currsmp_shunt_stm32_data_##n = {};                  \
 	DEVICE_DT_INST_DEFINE(n, &currsmp_shunt_stm32_init, NULL, &currsmp_shunt_stm32_data_##n,   \
 			      &currsmp_shunt_stm32_config_##n, PRE_KERNEL_1,                       \
 			      CONFIG_CURRSMP_INIT_PRIORITY, &currsmp_shunt_stm32_driver_api);
--- include/lib/motor/motor_Parameter.h
+++ include/lib/motor/motor_Parameter.h
@@ -3,53 +3,53 @@
 
 #if defined(CONFIG_MOTOR_SUPER_ABZHALL_400W)
 
-#define MOTOR_PAIRS		    (5.0f)
-#define MOTOR_RS		    (0.005f)
-#define MOTOR_LS		    (0.005f)
-#define MOTOR_DEBUG_IQ		    (0.02f)
-#define MOTOR_DEBUG_SELFANGLE	    (0.2f)
-#define SCETION_3_BASEANGLE	    (286.00f)
-#define SCETION_2_BASEANGLE	    (346.00f)
-#define SCETION_6_BASEANGLE	    (046.00f)
-#define SCETION_4_BASEANGLE	    (106.00f)
-#define SCETION_5_BASEANGLE	    (166.00f)
-#define SCETION_1_BASEANGLE	    (226.00f)
-#define HALL_SENSOR_POSITIVE_OFFSET (-30.0f)	   /* 霍尔传感器正方向偏移量 (度) */
-#define HALL_SENSOR_NEGATIVE_OFFSET (0.0f)	   /* 霍尔传感器负方向偏移量 (度) */
-#define ABZ_ENCODER_LINES	    (4096)	   // 编码器线数
-#define ABZ_ENCODER_RESOLUTION	    (0.087890625f) // 360.0f/ABZ_ENCODER_LINES
+#define MOTOR_PAIRS                 (5.0f)
+#define MOTOR_RS                    (0.005f)
+#define MOTOR_LS                    (0.005f)
+#define MOTOR_DEBUG_IQ              (0.02f)
+#define MOTOR_DEBUG_SELFANGLE       (0.2f)
+#define SCETION_3_BASEANGLE         (286.00f)
+#define SCETION_2_BASEANGLE         (346.00f)
+#define SCETION_6_BASEANGLE         (046.00f)
+#define SCETION_4_BASEANGLE         (106.00f)
+#define SCETION_5_BASEANGLE         (166.00f)
+#define SCETION_1_BASEANGLE         (226.00f)
+#define HALL_SENSOR_POSITIVE_OFFSET (-30.0f)       /* 霍尔传感器正方向偏移量 (度) */
+#define HALL_SENSOR_NEGATIVE_OFFSET (0.0f)         /* 霍尔传感器负方向偏移量 (度) */
+#define ABZ_ENCODER_LINES           (4096)         // 编码器线数
+#define ABZ_ENCODER_RESOLUTION      (0.087890625f) // 360.0f/ABZ_ENCODER_LINES
 
-#define TOQREMODE_D_KP	      (0.08f)
-#define TOQREMODE_D_KI	      (0.006f)
-#define TOQREMODE_D_KC	      (0.5f)
+#define TOQREMODE_D_KP        (0.08f)
+#define TOQREMODE_D_KI        (0.006f)
+#define TOQREMODE_D_KC        (0.5f)
 #define TOQREMODE_D_MAX_LIMIT (12.0f)
 #define TOQREMODE_D_MIN_LIMIT (-TOQREMODE_D_MAX_LIMIT)
 
-#define TOQREMODE_Q_KP	      (0.08f)
-#define TOQREMODE_Q_KI	      (0.006f)
-#define TOQREMODE_Q_KC	      (0.5f)
+#define TOQREMODE_Q_KP        (0.08f)
+#define TOQREMODE_Q_KI        (0.006f)
+#define TOQREMODE_Q_KC        (0.5f)
 #define TOQREMODE_Q_MAX_LIMIT (12.0f)
 #define TOQREMODE_Q_MIN_LIMIT (-TOQREMODE_Q_MAX_LIMIT)
 
-#define SPEEDMODE_SPEED_KP	  (0.0125f)
-#define SPEEDMODE_SPEED_KI	  (0.0083f)
-#define SPEEDMODE_SPEED_KC	  (0.5f)
+#define SPEEDMODE_SPEED_KP        (0.0125f)
+#define SPEEDMODE_SPEED_KI        (0.0083f)
+#define SPEEDMODE_SPEED_KC        (0.5f)
 #define SPEEDMODE_SPEED_MAX_LIMIT (48.0f)
 #define SPEEDMODE_SPEED_MIN_LIMIT (-TOQREMODE_D_MAX_LIMIT)
 
-#define SPEEDPLAN_MAX_A	 (100.00f)
-#define SPEEDPLAN_MIN_A	 (0.0f)
+#define SPEEDPLAN_MAX_A  (100.00f)
+#define SPEEDPLAN_MIN_A  (0.0f)
 #define SPEEDPLAN_MAX_JA (300.0f)
 #define SPEEDPLAN_MIN_JA (0.0f)
 
-#define POSMODE_POS_KP	      (10.0f)
-#define POSMODE_POS_KI	      (0.0001f)
-#define POSMODE_POS_KC	      (0.5f)
+#define POSMODE_POS_KP        (10.0f)
+#define POSMODE_POS_KI        (0.0001f)
+#define POSMODE_POS_KC        (0.5f)
 #define POSMODE_POS_MAX_LIMIT (2000.0f)
 #define POSMODE_POS_MIN_LIMIT (-POSMODE_POS_MAX_LIMIT)
 
 #define POSPLAN_MAX_SPEED (1400.0F)
-#define POSPLAN_MAX_ACC	  (3000.0f)
+#define POSPLAN_MAX_ACC   (3000.0f)
 #define POSPLAN_MAX_JACC  (15000.0f)
 
 #elif defined(CONFIG_MOTOR_ELUR_HALL)
@@ -59,49 +59,79 @@
 当前参数版本:速度最大到200rpm
 shell窗口最大输入 speed 18000
  */
-#define MOTOR_PAIRS		     (15.0f)
-#define MOTOR_DEBUG_IQ		     (0.02f)
-#define MOTOR_DEBUG_SELFANGLE	     (0.008f)
+#define MOTOR_PAIRS                  (15.0f)
+#define MOTOR_DEBUG_IQ               (0.02f)
+#define MOTOR_DEBUG_SELFANGLE        (0.008f)
 // #define MOTOR_ABSENCODER_TYPE_OFFSET (115.0f)
 #define MOTOR_ABSENCODER_TYPE_OFFSET (115.0f - 90)
 
-#define TOQREMODE_D_KP	      (0.4f)
-#define TOQREMODE_D_KI	      (0.08f)
-#define TOQREMODE_D_KC	      (0.5f)
+#define TOQREMODE_D_KP        (0.4f)
+#define TOQREMODE_D_KI        (0.08f)
+#define TOQREMODE_D_KC        (0.5f)
 #define TOQREMODE_D_MAX_LIMIT (12.0f)
 #define TOQREMODE_D_MIN_LIMIT (-TOQREMODE_D_MAX_LIMIT)
 
-#define TOQREMODE_Q_KP	      (0.4f)
-#define TOQREMODE_Q_KI	      (0.08f)
-#define TOQREMODE_Q_KC	      (0.5f)
+#define TOQREMODE_Q_KP        (0.4f)
+#define TOQREMODE_Q_KI        (0.08f)
+#define TOQREMODE_Q_KC        (0.5f)
 #define TOQREMODE_Q_MAX_LIMIT (12.0f)
 #define TOQREMODE_Q_MIN_LIMIT (-TOQREMODE_Q_MAX_LIMIT)
 
-#define SPEEDMODE_SPEED_KP	  (0.2f)  // TODO
-#define SPEEDMODE_SPEED_KI	  (0.01f) // TODO
-#define SPEEDMODE_SPEED_KC	  (0.5f)
+#define SPEEDMODE_SPEED_KP        (0.2f)  // TODO
+#define SPEEDMODE_SPEED_KI        (0.01f) // TODO
+#define SPEEDMODE_SPEED_KC        (0.5f)
 #define SPEEDMODE_SPEED_MAX_LIMIT (48.0f)
 #define SPEEDMODE_SPEED_MIN_LIMIT (-SPEEDMODE_SPEED_MAX_LIMIT)
 
-#define SPEEDPLAN_MAX_A	 (100.00f)
-#define SPEEDPLAN_MIN_A	 (0.0f)
+#define SPEEDPLAN_MAX_A  (100.00f)
+#define SPEEDPLAN_MIN_A  (0.0f)
 #define SPEEDPLAN_MAX_JA (300.0f)
 #define SPEEDPLAN_MIN_JA (0.0f)
 
-#define POSMODE_POS_KP	      (10.0f)
-#define POSMODE_POS_KI	      (0.0001f)
-#define POSMODE_POS_KC	      (0.5f)
+#define POSMODE_POS_KP        (10.0f)
+#define POSMODE_POS_KI        (0.0001f)
+#define POSMODE_POS_KC        (0.5f)
 #define POSMODE_POS_MAX_LIMIT (2000.0f)
 #define POSMODE_POS_MIN_LIMIT (-POSMODE_POS_MAX_LIMIT)
 
 #define POSPLAN_MAX_SPEED (1400.0F)
-#define POSPLAN_MAX_ACC	  (3000.0f)
+#define POSPLAN_MAX_ACC   (3000.0f)
 #define POSPLAN_MAX_JACC  (15000.0f)
 
 #elif defined(CONFIG_ENCODER_TYPE_AS5047)
-#define MOTOR_DEBUG_IQ	      (0.02f)
-#define MOTOR_DEBUG_SELFANGLE (0.2f)
+
+#define MOTOR_DEBUG_IQ               (0.02f)
+#define MOTOR_DEBUG_SELFANGLE        (0.2f)
+#define MOTOR_ABSENCODER_TYPE_OFFSET (115.0f - 90)
+#define TOQREMODE_D_KP               (0.4f)
+#define TOQREMODE_D_KI               (0.08f)
+#define TOQREMODE_D_KC               (0.5f)
+#define TOQREMODE_D_MAX_LIMIT        (12.0f)
+#define TOQREMODE_D_MIN_LIMIT        (-TOQREMODE_D_MAX_LIMIT)
+#define TOQREMODE_Q_KP               (0.4f)
+#define TOQREMODE_Q_KI               (0.08f)
+#define TOQREMODE_Q_KC               (0.5f)
+#define TOQREMODE_Q_MAX_LIMIT        (12.0f)
+#define TOQREMODE_Q_MIN_LIMIT        (-TOQREMODE_Q_MAX_LIMIT)
+#define SPEEDMODE_SPEED_KP           (0.2f)  // TODO
+#define SPEEDMODE_SPEED_KI           (0.01f) // TODO
+#define SPEEDMODE_SPEED_KC           (0.5f)
+#define SPEEDMODE_SPEED_MAX_LIMIT    (48.0f)
+#define SPEEDMODE_SPEED_MIN_LIMIT    (-SPEEDMODE_SPEED_MAX_LIMIT)
+#define SPEEDPLAN_MAX_A              (100.00f)
+#define SPEEDPLAN_MIN_A              (0.0f)
+#define SPEEDPLAN_MAX_JA             (300.0f)
+#define SPEEDPLAN_MIN_JA             (0.0f)
+#define POSMODE_POS_KP               (10.0f)
+#define POSMODE_POS_KI               (0.0001f)
+#define POSMODE_POS_KC               (0.5f)
+#define POSMODE_POS_MAX_LIMIT        (2000.0f)
+#define POSMODE_POS_MIN_LIMIT        (-POSMODE_POS_MAX_LIMIT)
+#define POSPLAN_MAX_SPEED            (1400.0F)
+#define POSPLAN_MAX_ACC              (3000.0f)
+#define POSPLAN_MAX_JACC             (15000.0f)
 #else
+
 #error "No valid board configuration selected!"
 #endif
 
